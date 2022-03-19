fc2a2f931805260b3fca83deef2d75c9ccc89064
Loïc Guillois <contact@loicguillois.fr>
2022-03-19 19:44:24 +0100

    add plugins

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,20 @@ fn add_player(mut commands: Commands) {
     commands.spawn().insert(Player).insert(Name("Zork".to_string()));
 }
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_player)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
+}
+
 fn main() {
     App::new()
-        .add_startup_system(add_player)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
 
