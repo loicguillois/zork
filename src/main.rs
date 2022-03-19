1a9bf1613d7adcb14d79d47bad5380c32aa27f91
Loïc Guillois <contact@loicguillois.fr>
2022-03-19 19:28:47 +0100

    Add bevy framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello World!");
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
 }
+
+fn main() {
+    App::new()
+        .add_system(hello_world)
+        .run();
+}
+
